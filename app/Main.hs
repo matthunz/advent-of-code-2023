23d41f5d54670bd00067f8f9af6f11dbcfe287eb
Matt Hunzinger <matthunz2@gmail.com>
2023-12-01 00:26:53 -0500

    Solve 1a

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
+import Data.Char (digitToInt, isDigit)
+import System.IO ( hGetContents, openFile, IOMode(ReadMode) )
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  n <- run "1" solve1A
+  print n
+
+run :: String -> (String -> a) -> IO a
+run name f = do
+  handle <- openFile ("input/" ++ name) ReadMode
+  contents <- hGetContents handle
+  pure $ f contents
+
+solve1A :: String -> Int
+solve1A s = sum $ map calibrationValue $ lines s
+
+calibrationValue :: String -> Int
+calibrationValue s = head digits * 10 + last digits
+  where
+    digits = map digitToInt $ filter isDigit s
